baadf4beca15f04e5ce5489a107f7fe1dc6600a2
L-Arscott <64332150+L-Arscott@users.noreply.github.com>
2022-07-02 10:29:35 +0100

    Added challenge 2-6
    Few additional minor changes from moving ECB oracle breaking to its own class

--- Cryptopals_challenges_Ch2.py
+++ Cryptopals_challenges_Ch2.py
@@ -15,9 +15,18 @@ C2_12 = AESCode('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg'
                 'dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg'
                 'YnkK', 'b64').ecb_encrypt('random')
 
-break_repeating(C2_12.oracle)
+C2_12_oracle = ECBOracle(C2_12.oracle_fun)
+C2_12_oracle.solve()
 
 ## Challenge 2-5
-C2_5 = AESCode(Profile('foo@baz').p, 'text')
+basic_profile_string = Profile('foo@baz').p
+C2_5 = AESCode(basic_profile_string, 'text')
 C2_5.cbc_encrypt('random', b'0123456789abcdef')
 print(string_to_dict(C2_5.cbc_solve()))
+
+## Challenge 2-6
+C2_6 = AESCode(basic_profile_string, 'text', True).ecb_encrypt('random')
+C2_6.prep = b'Yo1230123456789abcde'
+C2_6_oracle = ECBOracle(C2_6.oracle_fun)
+print(C2_6_oracle.l_full_prep_blocks)
+C2_6_oracle.solve()

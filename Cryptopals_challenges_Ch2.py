740c2685a39e03ab91ac07675c7726cbc20c2709
L-Arscott <64332150+L-Arscott@users.noreply.github.com>
2022-07-14 12:01:46 +0100

    Challenge 2.8 changes

--- Cryptopals_challenges_Ch2.py
+++ Cryptopals_challenges_Ch2.py
@@ -66,12 +66,12 @@ def challenge_2_8():
     c2_8_oracle = c2_8.gen_cbc_oracle(c2_8_str_fun)
 
     # Pass an empty block (that we don't mind scrambling) followed by block we wish to modify
-    encoded = c2_8_oracle(b'\x00' * 16 + b':admin<true:' + b'\x00' * 4)
     # Flip bits in the empty blocks, this scrambles the block and flips bits in the next when decoding
-    flipped = bit_flip(encoded, [32, 38, 43])
+    c2_8.easybyte = EasyByte(
+        c2_8_oracle(b'\x00' * 16 + b':admin<true:' + b'\x00' * 4))\
+        .bit_flip([32, 38, 43])
 
-    # Pass onto random cipher and solve
-    c2_8.easybyte.b = flipped
+    # Solve
     assert b';admin=true;' in c2_8.cbc_solve()
     print('Challenge 2-8 passed !')
 

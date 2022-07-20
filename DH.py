42b41768b1fde175ab317d6109ca2614f6e3be65
L-Arscott <64332150+L-Arscott@users.noreply.github.com>
2022-07-20 13:18:22 +0100

    Add files via upload
    Diffie-Hellman file
    SHA-1 file

---
+++ DH.py
@@ -0,0 +1,209 @@
+"""
+Classes related to Diffie-Hellman key exchange
+"""
+
+from random import randint
+from Cryptopals_main import AESCode
+from SHA_1 import sha_1
+
+# Large number operations
+def power_mod(b, e, m):
+    # Calculates b**e % m, useful for large numbers
+    x = 1
+    while e > 0:
+        # Calculate b to the power of powers of two
+        # Simulatneously decompose e into powers of two
+        b, e, x = (
+            b * b % m,
+            e // 2,
+            b * x % m if e % 2 else x
+        )
+
+    return x
+
+# Classes
+class DHSender:
+    """Sender in a standard Diffie-Hellman key exchange protocol
+    DHSender will agree upon a key with DHReceiver
+    DHSender can send an ecnrypted message and receive and decrypt a reply
+
+        Attributes
+        ----------
+        msg_to_send: str
+            Message to be sent via encryption
+        p: int
+            Large prime number
+        g: int
+            A primitive root mod p
+        a: int
+            A random integer mod p.
+            This integer is kept private.
+        A: int
+            A = g**a mod p
+            This integer will be public.
+        B: int
+            To be declared by the receiver.
+            B = g**b mod p, where b is a random integer mod p kept secret by the receiver
+        s: int
+            Once B is declared by the receiver, sender can generate s = B**a mod p
+            The receiver generates the same number s via s = A**b mod p
+        key: bytes
+            A key obtained by hashing s in some fashion
+        iv: bytes
+            Initialisation vector used for CBC encryption
+        encode: AESCode
+            Holds cipher and message to be encoded.
+            Generates the cipher text.
+        rec_msg: bytes
+            Reply sent by receiver (encrypted using agreed upon cipher)
+        decode: AESCode
+            Holds cipher and rec_msg.
+            Used to decode rec_msg.
+
+        Parameters
+        ----------
+        msg: str
+            Message to be sent via encryption
+        p: int
+            Large prime number
+        g: int
+            A primitive root mod p
+    """
+    def __init__(self, msg: str,
+                 p=int('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc'
+                       '74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f'
+                       '14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f4'
+                       '06b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8'
+                       'a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f3'
+                       '56208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffff'
+                       'ffffffffffff', 16),
+                 g=2):
+        self.msg_to_send = msg
+        self.p = p
+        self.g = g
+        self.a = randint(0, p - 1)
+        self.A = power_mod(g, self.a, p)
+        self.B = None
+        self.s = None
+        self.key = None
+        self.iv = None
+        self.encode = None
+        self.rec_msg = None
+        self.decode = None
+
+    def gen_encode(self):
+        # Generates AESCode instance holding message to be encrypted along with cipher
+        # AESCode method self.cbc_encrypt() will be used to generate cipher text
+        code = AESCode(self.msg_to_send, 'text', key=self.key, iv='random')
+        return code
+
+    def gen_decode(self):
+        # Generates AESCode instance holding message to be deciphered along with cipher
+        # AESCode method self.solve() will be used to obtain plaintext
+        code = AESCode(self.rec_msg, key=self.key, iv=self.iv)
+
+        return code
+
+    def gen_s(self):
+        # Returns integer sender and receiver have agreed upon,
+        # from which a key will be hashed
+        return power_mod(self.B, self.a, self.p)
+
+    def gen_key(self):
+        # From the integer self.s, generates a key to be used for encryption
+        int_as_bytes = self.s.to_bytes(192, 'big')  # Transform integer to bytes
+
+        return sha_1(int_as_bytes)[0:16]  # Hash and return 16-byte key
+
+class DHReceiver:
+    """Receiver in a standard Diffie-Hellman key exchange protocol
+        DHReceiver will agree upon a key with DHSender
+        DHReceiver can receive and decrypt an ecnrypted from DHSender
+        DHReceiver can send a reply
+
+        Attributes
+        ----------
+        p: int
+            Large prime number
+        g: int
+            A primitive root mod p
+        b: int
+            A random integer mod p.
+            This integer is kept private.
+        B: int
+            A = g**b mod p
+            This integer will be public.
+        A: int
+            To be declared by the sender.
+            A = g**a mod p, where a is a random integer mod p kept secret by the sender
+        s: int
+            Once A is declared by the sender, receiver can generate s = A**b mod p
+            The sender generates the same number s via s = B**a mod p
+        key: bytes
+            A key obtained by hashing s in some fashion
+        iv: bytes
+            Initialisation vector used for CBC encryption, chosen by sender
+        rec_msg: bytes
+            Message sent by sender (encrypted using agreed upon cipher)
+        decode: AESCode
+            Holds cipher and rec_msg.
+            Used to decode rec_msg.
+        reply: str
+            Receiver's reply
+        encode: AESCode
+            Holds cipher and reply to be encoded.
+            Generates the cipher text.
+
+        Parameters
+        ----------
+        p: int
+            Large prime number
+        g: int
+            A primitive root mod p
+    """
+    def __init__(self,
+                 p=int('ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024e088a67cc'
+                       '74020bbea63b139b22514a08798e3404ddef9519b3cd3a431b302b0a6df25f'
+                       '14374fe1356d6d51c245e485b576625e7ec6f44c42e9a637ed6b0bff5cb6f4'
+                       '06b7edee386bfb5a899fa5ae9f24117c4b1fe649286651ece45b3dc2007cb8'
+                       'a163bf0598da48361c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f3'
+                       '56208552bb9ed529077096966d670c354e4abc9804f1746c08ca237327ffff'
+                       'ffffffffffff', 16),
+                 g=2):
+        self.p = p
+        self.g = g
+        self.b = randint(0, p - 1)
+        self.B = power_mod(g, self.b, p)
+        self.A = None
+        self.s = None
+        self.key = None
+        self.iv = None
+        self.rec_msg = None
+        self.decode = None
+        self.reply = None
+        self.encode = None
+
+    def gen_decode(self):
+        # Generates AESCode instance holding message to be deciphered along with cipher
+        # AESCode method self.solve() will be used to obtain plaintext
+        code = AESCode(self.rec_msg, key=self.key, iv=self.iv)
+
+        return code
+
+    def gen_encode(self):
+        # Generates AESCode instance holding message to be encrypted along with cipher
+        # AESCode method self.cbc_encrypt() will be used to generate cipher text
+        code = AESCode(self.reply, 'text', key=self.key, iv=self.iv)
+
+        return code
+
+    def gen_s(self):
+        # Returns integer sender and receiver have agreed upon,
+        # from which a key will be hashed
+        return power_mod(self.A, self.b, self.p)
+
+    def gen_key(self):
+        # From the integer self.s, generates a key to be used for encryption
+        int_as_bytes = self.s.to_bytes(192, 'big')  # Transform integer to bytes
+
+        return sha_1(int_as_bytes)[0:16]  # Hash and return 16-byte key
---
+++ SHA_1.py
@@ -0,0 +1,96 @@
+"""
+Implementation of the pseudocode for the SHA-1 has function, found at
+https://en.wikipedia.org/wiki/SHA-1#SHA-1_pseudocode
+
+@author: Lawrence Arscott
+"""
+
+from MT19937 import IntAsWord
+
+# Operations
+def bit_not(n, numbits=8):
+    # Returns not n, where n is a 'numbits' bits integer
+    return (1 << numbits) - 1 - n
+
+# Main
+def sha_1(msg: bytes):
+    # Declare variables
+    h0 = 0x67452301
+    h1 = 0xEFCDAB89
+    h2 = 0x98BADCFE
+    h3 = 0x10325476
+    h4 = 0xC3D2E1F0
+
+    ml = len(msg) * 8  # Length of message in bits
+
+    # Pre-processing
+    msg += b'\x80'
+
+    # Append 0 ≤ k < 512 bits '0', such that the resulting message length in bits
+    # is congruent to −64 ≡ 448 (mod 512)
+    msg += ((56 - len(msg)) % 64) * b'\x00'
+
+    # Append ml, the original message length in bits, as a 64-bit big-endian integer.
+    # Thus, the total length is a multiple of 512 bits/64 bytes.
+    msg += ml.to_bytes(8, 'big')
+    assert len(msg) % 64 == 0
+
+    # Process the message in successive 512-bit chunks:
+    # Break message into 512-bit chunks
+    chunks = [msg[i: i+64] for i in range(0, len(msg), 64)]
+
+    # Break chunk into sixteen 32-bit big-endian words w[i], 0 ≤ i ≤ 15
+    for chunk in chunks:
+        words = [int.from_bytes(chunk[i: i+4], 'big') for i in range(0, 64, 4)]
+
+        # Message schedule: extend the sixteen 32-bit words into eighty 32-bit words:
+        for i in range(16, 80):
+            new_word = words[i-3] ^ words[i-8] ^ words[i-14] ^ words[i-16]
+            new_word = IntAsWord(new_word, 32).lrot(1).as_int
+            words.append(new_word)
+        
+        # Initialize hash value for this chunk:
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
+        
+        # Main loop:
+        for i in range(80):
+            if 0 <= i <= 19:
+                f = (b & c) | ((bit_not(b, 32)) & d)
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d)
+                k = 0x8F1BBCDC
+            else:
+                try:
+                    assert 60 <= i <= 79
+                except AssertionError:
+                    print('i value error in main loop')
+
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
+
+            temp = IntAsWord(a, 32).lrot(5).as_int + f + e + k + words[i] % 2 ** 32
+            e = d
+            d = c
+            c = IntAsWord(b, 32).lrot(30).as_int
+            b = a
+            a = temp % 2 ** 32
+
+        # Add this chunk's hash to result so far:
+        h0 = (h0 + a) % 2 ** 32
+        h1 = (h1 + b) % 2 ** 32
+        h2 = (h2 + c) % 2 ** 32
+        h3 = (h3 + d) % 2 ** 32
+        h4 = (h4 + e) % 2 ** 32
+
+        # Produce the final hash value (big-endian) as a 160-bit number:
+        hh = ((h0 << 128) | (h1 << 96) | (h2 << 64) | (h3 << 32) | h4) % (2 ** 160)
+
+        return hh.to_bytes(20, 'big')

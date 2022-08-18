9df68a8121ca7ffc77695667a9c071fc4d8a4988
L-Arscott <64332150+L-Arscott@users.noreply.github.com>
2022-08-18 17:53:15 +0100

    Changes in line with DH

--- Cryptopals_challenges_Ch8/Challenge_58/Eve_8-2.py
+++ Cryptopals_challenges_Ch8/Challenge_58/Eve_8-2.py
@@ -5,15 +5,18 @@
 import socket
 from DH import *
 from Cryptopals_main import crt
+from Group import ModP
 
-our_q = 236234353446506858198510045061214171961
+our_q = 335062023296420808191071248367701059461
 our_p = int('DB020645333C52A8D8BD194950CBD48DDF752BAE8F346150C6410DBA6BEFDBC6CF93D7CFC4568FFB017B2'
             '8BEF26242493C606596B7FF8625055F73E888B86117', 16)
 our_g = int('BE4ED76592B0FC7A8F2A160840C664BD8A4E0DFF8DED0B2ED0843714C3B7BD12EE50CB56A829A999CA957'
             '14A520BA0C080E7A5866309E4BBCCE1F897EAFB77D', 16)
 
+our_group = ModP(p=our_p, g=our_g, q=our_q)
+
 # Initiate Eve
-eve = DHAttacker(p=our_p, g=our_g, q=our_q)
+eve = DHAttacker(our_group)
 
 # Some small factors of (p-1) / q
 factors = [2, 12457, 14741, 18061, 31193, 33941, 63803]
@@ -37,12 +40,13 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         # Obtain secret key mod factor for each factor
         for j in range(len(factors)):
             factor = factors[j]
-            assert (eve.p - 1) % factor == 0  # Check validity of factor
+            assert (our_p - 1) % factor == 0  # Check factor divides order of group
 
             # Generate h, an element of order factor
             h = 1
             while h == 1:
-                h = power_mod(randint(2, eve.p - 1), (eve.p - 1) // factor, eve.p)
+                guess = randint(2, our_p - 1)
+                h = our_group.scale((our_p - 1) // factor, guess)
 
             # Eve sends h
             to_send = str(h).encode()  # Convert to bytes
@@ -61,7 +65,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             # Brute force the mac, only 'factor' possibilities
             i = 0
             while powers[j] is None and i < factor:
-                potential_key = sha256(str(power_mod(h, i, eve.p)).encode()).digest()[:16]
+                eve.s = our_group.scale(i, h)
+                potential_key = eve.gen_key()
                 if check_hmac(eve.rec_msg, potential_key):
                     # noinspection PyTypeChecker
                     powers[j] = i  # We have cracked secret key mod factor

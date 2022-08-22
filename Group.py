91f8be0dd0e144a0e111e0864de30f5c05dc3f74
L-Arscott <64332150+L-Arscott@users.noreply.github.com>
2022-08-22 22:22:46 +0100

    Added elliptic curves and cyclic groups

--- Group.py
+++ Group.py
@@ -2,6 +2,7 @@
 
 import copy
 from math import isqrt
+from random import randint
 
 
 def g_el_to_scalar(y, k):
@@ -9,27 +10,31 @@ def g_el_to_scalar(y, k):
     p_rand = pow(2, y, k)
     return p_rand if p_rand != 0 else 1
 
-class AbGroup:
+
+class Group:
+    """Base class for groups
+
+    Attributes
+    ----------
+    id: int = 1
+        Identity element
+
+    Parameters
+    ----------
+    """
     def __init__(self):
         self.add = self._trivial_add
         self.id = 1
-        self.g = 1
-        self.q = 1
 
     @staticmethod
     def _trivial_add(g1, g2):
         # Trivial group operation
         return g1 * g2
 
-    def new_element(self, new_g):
-        new = copy.deepcopy(self)
-        new.g = new_g
-        return new
-
-    def scale(self, k, g=None):
+    def scale(self, g, k):
         # Returns self.g ** k
         result = copy.deepcopy(self.id)
-        x = copy.deepcopy(self.g) if g is None else g
+        x = g
 
         while k > 0:
             if k % 2 == 1:
@@ -39,16 +44,21 @@ class AbGroup:
 
         return result
 
-    @staticmethod
-    def prep_key(key_bytes):
-        print('This should not be called')
+    def gen_order(self, desired_order: int, grp_order: int):
+        # Generate an element of order desired_order
+        h = self.id
+        while h == self.id:
+            guess = randint(2, grp_order)
+            h = self.scale(guess, grp_order // desired_order)
+
+        return h
 
     # TODO: This is ModP centric
     # noinspection PyPep8Naming
-    def disc_log(self, start: int, end: int, y, f: callable = g_el_to_scalar):
+    def disc_log(self, start: int, end: int, g, y, f: callable = g_el_to_scalar):
         """
         Pollard's Method for Catching Kangaroos
-        Find x such that g**x mod m = y, with the knowledge that x is between start and end.
+        Find x such that g**x = y, with the knowledge that x is between start and end.
         Context: Cyclic groups, ex: multiplication of integers mod m
 
         Parameters
@@ -57,7 +67,9 @@ class AbGroup:
             Lowest suspected integer such that g**x = y
         end : int
             Greatest suspected integer such that g**x = y
-        y: int
+        g
+            Group element we took a power of
+        y
             Group element we are trying to invert
         f: callable
             Pseudorandom function f:G->S taking group elements to scalars
@@ -77,7 +89,7 @@ class AbGroup:
 
         # Tame kangaroo
         xT = 0
-        yT = self.scale(end)
+        yT = self.scale(g, end)
 
         # Wild kangaroo
         xW = 0
@@ -88,7 +100,7 @@ class AbGroup:
         # Tame kangaroo jumps
         for _ in range(big_n):
             jump = f2(yT)  # Function gives us pseudorandom jumpsize
-            yT = self.add(yT, self.scale(jump))  # Kangaroo jumps
+            yT = self.add(yT, self.scale(g, jump))  # Kangaroo jumps
             xT += jump  # Keep track how far kangaro has jumped (yT *= xT so far)
 
             # Show progress every so many jumps
@@ -100,14 +112,14 @@ class AbGroup:
         # Distance to cover is between xT and n + xT
         while xW < n + xT:
             jump = f2(yW)  # Function gives us pseudorandom jumpsize
-            yW = self.add(yW, self.scale(jump))  # Kangaroo jumps
+            yW = self.add(yW, self.scale(g, jump))  # Kangaroo jumps
             xW += jump  # Keep track how far kangaro has jumped (yW *= xW so far)
 
             # Check whether paths have coincided
             # yW will either land on yT or skip past
             if yW == yT:
                 ans = end + xT - xW
-                assert self.scale(ans) == y  # Check our answer
+                assert self.scale(g, ans) == y  # Check our answer
                 print(ans)
                 return ans
 
@@ -120,14 +132,26 @@ class AbGroup:
         print('Kangaroo has not been caught')
 
 
-class ModP(AbGroup):
-    def __init__(self, p: int, g: int = 1, q=None):
+class ModP(Group):
+    """Multiplicative group of integers mod p
+
+    Attributes
+    ----------
+    p: int
+        Integers are taken mod p
+    id: int = 1
+        Identity element
+
+    Parameters
+    ----------
+    p: int
+        Integers are taken mod p
+    """
+    def __init__(self, p: int):
         super().__init__()
         self.p = p
         self.add = self._create_mod_mult(p)
         self.id = 1
-        self.g = g
-        self.q = q if q is not None else p
 
     @staticmethod
     def _create_mod_mult(p):
@@ -139,22 +163,126 @@ class ModP(AbGroup):
 
         return mod_mult
 
-    @staticmethod
-    def prep_key(g):
-        # Turns a group element into bytes
-        int_as_bytes = str(g).encode()  # Transform integer to bytes
 
-        return int_as_bytes
+class EGroup(Group):
+    """Class for points on an elliptic curve over the finite field F_p.
+    These points are endowed with an abelian group structure.
+    To be used in elliptic curve Diffie-Hellman key exchange
 
-##
-test_p = 11470374874925275658116663507232161402086650258453896274534991676898999262641581519101074740642369848233294239851519212341844337347119899874391456329785623
-test_q = 335062023296420808191071248367701059461
-test_j = 34233586850807404623475048381328686211071196701374230492615844865929237417097514638999377942356150481334217896204702
-test_g = 622952335333961296978159266084741085889881358738459939978290179936063635566740258555167783009058567397963466103140082647486611657350811560630587013183357
-test_y = 7760073848032689505395005705677365876654629189298052775754597607446617558600394076764814236081991643094239886772481052254010323780165093955236429914607119
+    Attributes
+    ----------
+    p: int
+        Degree of the field F_p
+    a: int
+        Coefficient in the equation of the curve
+        y^2 = x^3 + ax + b
+    b: int
+        Constant in the equation of the curve
+        y^2 = x^3 + ax + b
+    id: str = 'O'
+        Identity element
 
-test_group = ModP(test_p, test_g, test_q)
-test_group.disc_log(0, 2**20, test_y)
+    Parameters
+    ----------
+    p: int
+        Prime number that will be the degree of the field F_p
+    a: int
+        Coefficient of x in the equation of the curve
+        y^2 = x^3 + ax + b
+    b: int
+        Constant in the equation of the curve
+        y^2 = x^3 + ax + b
+    """
+    def __init__(self, p: int, a: int, b: int):
+        super().__init__()
+        self.p = p
+        self.a = a
+        self.b = b
+        self.id = 'O'
+        self.add = self.e_add
 
-##
+    def check(self, pt) -> bool:
+        # Returns True/False depending on whether pt is on the curve
+        x, y = pt
 
+        lhs = y**2 % self.p
+        rhs = (x**3 + self.a * x + self.b) % self.p
+
+        return lhs == rhs
+
+    def gen_y_squared(self, x):
+        y_squared = (x**3 + self.a * x + self.b) % self.p
+        print(y_squared)
+
+    def _mod_inv(self, x):
+        # Inverts x mod self.p
+        return pow(x, self.p - 2, self.p)
+
+    def inverse(self, pt):
+        # Inverts group element pt
+        x, y = pt
+
+        return x, self.p - y
+
+    def e_add(self, pt, other_pt):
+        # Add two group elements pt and other_pt together
+        if pt == 'O':
+            return other_pt
+
+        if other_pt == 'O':
+            return pt
+
+        if self.inverse(pt) == other_pt:
+            return 'O'
+
+        x1, y1 = pt
+        x2, y2 = other_pt
+
+        if pt == other_pt:
+            m = ((3 * x1 ** 2 + self.a) * self._mod_inv(2 * y1))
+
+        else:
+            m = (y2 - y1) * self._mod_inv(x2 - x1)
+
+        x3 = (m ** 2 - x1 - x2) % self.p
+        y3 = (m * (x1 - x3) - y1) % self.p
+
+        return x3, y3
+
+
+class CycGroup(Group):
+    """Cyclic group
+
+    Attributes
+    ----------
+    q: int
+        Order of the group
+    id
+        Identity element
+    g
+        Generator of the group
+
+    Parameters
+    ----------
+    order: int
+        Order of the group
+    identity
+        Identity element
+    g
+        Generator of the group
+    add_fun: callable
+        Function defining how to add two group elements
+    """
+    def __init__(self, order: int, identity, g, add_fun: callable):
+        super().__init__()
+        self.add = add_fun
+        self.q = order
+        self.id = identity
+        self.g = g
+
+    @classmethod
+    def from_generator(cls, group: Group, element, order: int):
+        # Cyclic group is generated from an element of another group
+        # This group will have order the order of the element
+        return cls(order, group.id, element, group.add)
+    
